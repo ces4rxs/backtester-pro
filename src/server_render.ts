6ac6c39e8ccabf274c0f3dd8be4a7d33fcacebe6
Tu Nombre <tuemail@example.com>
2025-10-29 17:12:39 -0500

    fix: ruta correcta a routes/server_strategies para Render

--- src/server_render.ts
+++ src/server_render.ts
@@ -5,7 +5,7 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 
 // 🧩 Importa el router de estrategias
-import strategiesRouter from "../routes/server_strategies";
+import strategiesRouter from "./routes/server_strategies.js";
 
 dotenv.config();
 
